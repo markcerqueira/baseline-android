a33abb905960436aa880ead7b5f1f8e14f0e497c
Mark Cerqueira <mark.cerqueira@gmail.com>
2017-02-08 21:16:23 -0800

    Add husk for NetworkingFragment

--- app/src/main/java/gg/mark/baselineandroid/MainActivity.java
+++ app/src/main/java/gg/mark/baselineandroid/MainActivity.java
@@ -1,10 +1,11 @@
 package gg.mark.baselineandroid;
 
+import android.app.Fragment;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -12,6 +13,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+
+import java.util.HashSet;
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -74,14 +79,22 @@ public class MainActivity extends AppCompatActivity
         return super.onOptionsItemSelected(item);
     }
 
+    private static final String NETWORKING_FRAGMENT_TAG = "NETWORKING_FRAGMENT_TAG";
+
+    private static final Set<String> sFragmentTagsSet;
+    static {
+        sFragmentTagsSet = new HashSet<>();
+        sFragmentTagsSet.add(NETWORKING_FRAGMENT_TAG);
+    }
+
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
+        if (id == R.id.networking) {
+            showFragment(new NetworkFragment(), NETWORKING_FRAGMENT_TAG);
         } else if (id == R.id.nav_gallery) {
 
         } else if (id == R.id.nav_slideshow) {
@@ -97,5 +110,20 @@ public class MainActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    private void showFragment(Fragment fragment, String tag) {
+        android.app.FragmentManager fm = getFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+
+        // Remove all previous fragments
+        for (String fragmentTag : sFragmentTagsSet) {
+            Fragment existingFragment = fm.findFragmentByTag(fragmentTag);
+            if (existingFragment != null) {
+                ft.remove(existingFragment);
+            }
+        }
+
+        ft.add(R.id.main_content_view, fragment,tag).commit();
     }
 }
